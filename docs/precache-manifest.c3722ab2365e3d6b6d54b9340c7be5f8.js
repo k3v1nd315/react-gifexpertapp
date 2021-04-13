3064256536a9309949b131a3db2f64ea3832d57d
Kevin Dels <k3v1nd315@gmail.com>
2021-04-13 09:32:45 -0500

    docs agregado

---
+++ docs/precache-manifest.c3722ab2365e3d6b6d54b9340c7be5f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c7e6443bbef165bc4f052e21d5eb0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb75c209769e4685e19a",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb75c209769e4685e19a",
+    "url": "/static/js/main.acf46ac4.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
